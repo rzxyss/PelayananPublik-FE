0fc38b76e874d94286a764690fde2bcdbbcc285b
Rizki Saepul Aziz <rzkysa0@gmail.com>
2023-01-19 15:35:15 +0700

    edit admin page

--- pages/admin/login.js
+++ pages/admin/login.js
@@ -15,7 +15,7 @@ export default function Login() {
       const res = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/login`, {
         username: inputUsername,
         password: inputPassword,
-        token: token
+        token: token,
       });
       if (res.data.length > 0) {
         if (
@@ -54,35 +54,26 @@ export default function Login() {
         <div className="w-full max-w-md space-y-8">
           <div>
             <img
-              className="mx-auto h-12 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              alt="Your Company"
+              className="mx-auto h-20 w-auto"
+              src="/image/logo.png"
+              alt="Tikomdik Ceria"
             />
-            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-              Sign in to your account
+            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-black/80">
+              Admin Login
             </h2>
-            <p className="mt-2 text-center text-sm text-gray-600">
-              Or
-              <Link
-                href="/auth/register"
-                className="font-medium text-indigo-600 hover:text-indigo-500"
-              >
-                Sign up
-              </Link>
-            </p>
           </div>
           <form onSubmit={handleSubmit} className="mt-8 space-y-6">
             <input type="hidden" name="remember" defaultValue="true" />
             <div className="-space-y-px rounded-md shadow-sm">
               <div>
-                <label htmlFor="password" className="sr-only">
+                <label htmlFor="username" className="sr-only">
                   Username
                 </label>
                 <input
                   value={inputUsername}
                   onChange={(e) => setUsername(e.target.value)}
                   type="text"
-                  className="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  className="relative block w-full appearance-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-[#112883] focus:outline-none focus:ring-[#112883] sm:text-sm"
                   placeholder="Username"
                 />
                 <label htmlFor="password" className="sr-only">
@@ -91,11 +82,8 @@ export default function Login() {
                 <input
                   value={inputPassword}
                   onChange={(e) => setPassword(e.target.value)}
-                  name="inputPassword"
-                  type="text"
-                  autoComplete="current-password"
-                  required
-                  className="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  type="password"
+                  className="relative block w-full appearance-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-[#112883] focus:outline-none focus:ring-[#112883] sm:text-sm"
                   placeholder="Password"
                 />
               </div>
@@ -103,7 +91,7 @@ export default function Login() {
             <div>
               <button
                 type="submit"
-                className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                className="group relative flex w-full justify-center rounded-md border border-transparent bg-[#112883] py-2 px-4 text-sm font-medium text-white hover:bg-[#112883] focus:outline-none focus:ring-2 focus:ring-[#112883] focus:ring-offset-2"
               >
                 Sign in
               </button>
