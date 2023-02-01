e5ac8bdd9b787352045db567cf407651c818eac3
Rizki Saepul Aziz <rzkysa0@gmail.com>
2023-02-01 14:17:21 +0700

    add FaQ pages

---
+++ pages/faq.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+
+export default function FaQ() {
+  return (
+    <div className='w-full h-auto'>
+      <Navbar />
+      <Footer />
+    </div>
+  )
+}
