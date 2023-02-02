01737f33d60e1da1aca906da5f61866f08daf8ed
Rizki Saepul Aziz <rzkysa0@gmail.com>
2023-02-02 15:30:40 +0700

    rework footer

--- components/Footer.js
+++ components/Footer.js
@@ -3,19 +3,26 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="w-full h-auto border-t">
-      <div className="flex flex-col items-center py-5 space-y-3">
-        <Image
-          src={"/image/logo.png"}
-          responsive="true"
-          alt="Logo TikomDik"
-          width={200}
-          height={0}
-        />
-        <h1 className="font-Poppins font-normal text-md">&copy; Copyright 2023 All Rights Reserved by Team SMKN 13 Bandung</h1>
-        <h1 className="font-Poppins font-normal text-md">Jl. Dr. Rajiman No.6, Pasir Kaliki, Kec. Cicendo</h1>
-        <h1 className="font-Poppins font-normal text-md">Bandung, Jawa Barat 40171, Telp.0838-2063-0348</h1>
+    <footer className="bg-gray-800 text-white py-4">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+        <p className="text-center md:text-left">
+          &copy; {new Date().getFullYear()} Your Company
+        </p>
+        <div className="flex">
+          <a
+            href="#"
+            className="px-4 py-2 hover:bg-gray-700 rounded-lg"
+          >
+            About
+          </a>
+          <a
+            href="#"
+            className="px-4 py-2 hover:bg-gray-700 rounded-lg"
+          >
+            Contact
+          </a>
+        </div>
       </div>
-    </div>
+    </footer>
   );
 }
