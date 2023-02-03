1436077edc546abc2f452d2702eee33461353de6
Rizki Saepul Aziz <rzkysa0@gmail.com>
2023-02-03 14:43:15 +0700

    add like system bad version

--- pages/berita.js
+++ pages/berita.js
@@ -1,31 +1,64 @@
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
-import BeritaPopular from "../components/BeritaPopular";
-import BeritaBaru from "../components/BeritaBaru";
 import { MdHistory, MdPeople } from "react-icons/md";
 import { BiSearch } from "react-icons/bi";
+import { AiOutlineLike } from "react-icons/ai";
 import axios from "axios";
 import Footer from "../components/Footer";
+import Swal from "sweetalert2";
 
 export default function Berita() {
-  const [tabBerita, setTabBerita] = useState("popular");
   const [berita, setBerita] = useState([]);
   const [limit, setLimit] = useState(9);
+  const [query, setQuery] = useState("");
+  const [likeCount, setLike] = useState();
 
   const getBerita = async () => {
     const berita = await axios.get(
-      `${process.env.NEXT_PUBLIC_API_URL}/berita?limit=${limit}`
+      `${process.env.NEXT_PUBLIC_API_URL}/berita?limit=${limit}&search_query=${query}`
     );
     setBerita(berita.data.results);
   };
 
-  // console.log(limit)
-  // onClick={() => setLimit(50)}
+  const likeBerita = async (beritaId) => {
+    try {
+      const res = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_URL}/berita/${beritaId}`
+      );
+      setLike(res.data.like);
+      if (!res) {
+        Swal.fire({
+          position: "center",
+          icon: "error",
+          title: "Gagal Menambahkan Like",
+          showConfirmButton: false,
+          timer: 2000,
+        });
+      } else {
+        await axios.patch(
+          `${process.env.NEXT_PUBLIC_API_URL}/likeberita/${beritaId}`,
+          {
+            like: res.data.like + 1,
+          }
+        );
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          title: "Berhasil Menambahkan Like",
+          showConfirmButton: false,
+          timer: 2000,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    setLike(berita.like);
+  };
 
   useEffect(() => {
     getBerita();
-  }, []);
+  }, [query, likeCount]);
   return (
     <div className="w-full h-auto">
       <Navbar />
@@ -47,15 +80,17 @@ export default function Berita() {
                 </h1>
 
                 <form className="lg:w-2/12">
-                  <div class="relative">
+                  <div className="relative">
                     <input
                       type="search"
-                      class="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-black focus:border-black focus:outline-none"
+                      className="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-black focus:border-black focus:outline-none"
                       placeholder="Cari ..."
+                      value={query}
+                      onChange={(e) => setQuery(e.target.value)}
                     />
                     <button
                       type="submit"
-                      class="text-white absolute right-2.5 bottom-2.5 bg-[#112883] hover:bg-[#112883]/90 font-medium rounded-lg text-sm px-4 py-2"
+                      className="text-white absolute right-2.5 bottom-2.5 bg-[#112883] hover:bg-[#112883]/90 font-medium rounded-lg text-sm px-4 py-2"
                     >
                       <BiSearch className="w-5 h-5" />
                     </button>
@@ -67,7 +102,7 @@ export default function Berita() {
                   {berita.map((data, index) => {
                     return (
                       <div
-                        className="hover:scale-105 hover:bg-black/10 p-3 rounded-lg duration-500"
+                        className="hover:bg-black/10 p-3 rounded-lg duration-500"
                         key={index}
                       >
                         <Image
@@ -86,11 +121,22 @@ export default function Berita() {
                             {data.deskripsi_berita}
                           </h1>
                         </div>
-                        <div className="flex flex-row items-center text-black/60 mt-2">
-                          <MdHistory className="w-5 h-5" />
-                          <h1 className="font-Poppins font-light text-sm">
-                            {data.createdAt}
-                          </h1>
+                        <div className="flex justify-between px-2">
+                          <div className="flex flex-row items-center text-black/60 mt-2 gap-1">
+                            <MdHistory className="w-5 h-5" />
+                            <h1 className="font-Poppins font-light text-sm">
+                              {data.createdAt}
+                            </h1>
+                          </div>
+                          <div className="flex flex-row items-center text-black/60 mt-2 gap-1 cursor-pointer">
+                            <AiOutlineLike
+                              className="w-10 h-10"
+                              onClick={() => likeBerita(data.id)}
+                            />
+                            <h1 className="font-Poppins font-light text-lg">
+                              {data.like || 0}
+                            </h1>
+                          </div>
                         </div>
                       </div>
                     );
@@ -98,9 +144,21 @@ export default function Berita() {
                 </div>
               </div>
               <div className="w-full flex justify-center pb-5">
-                <button className="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded" onClick={() => setLimit(50)}>
-                  Muat Lebih Banyak
-                </button>
+                {limit != 50 ? (
+                  <button
+                    className="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded"
+                    onClick={() => setLimit(50)}
+                  >
+                    Muat Lebih Banyak
+                  </button>
+                ) : (
+                  <button
+                    className="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded"
+                    onClick={() => setLimit(9)}
+                  >
+                    Muat Lebih Sedikit
+                  </button>
+                )}
               </div>
             </div>
           </div>
