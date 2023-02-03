af3213828ed267425ccc056cbe0de88e01cd3cf3
Rizki Saepul Aziz <rzkysa0@gmail.com>
2023-02-03 14:42:45 +0700

    add get berita terbaru dan popular dari BE

--- components/BeritaBaru.js
+++ components/BeritaBaru.js
@@ -1,26 +1,38 @@
-import React from 'react'
-import {
-  MdHistory,
-  MdPeople
-} from 'react-icons/md'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { MdHistory } from "react-icons/md";
 
 export default function BeritaBaru() {
+  const [berita, setBerita] = useState([]);
+  const getBerita = async () => {
+    try {
+      const results = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_URL}/beritaterbaru`
+      );
+      setBerita(results.data);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    getBerita();
+  }, []);
   return (
-    <>
-      <div className="p-5 hover:scale-105 hover:bg-black/10 rounded-lg duration-500">
-        <h1 className="font-Poppins font-normal text-lg">Lorem ipsum dolor sit amet consectetur. Pulvinar viverra dictumst eleifend sed suspendisse quis. Habitasse sit ornare sed neque.</h1>
-        <div className="flex flex-row items-center text-black/60 mt-2">
-          <MdHistory className="w-5 h-5" />
-          <h1 className="font-Poppins font-light text-sm">Berapa Hari Yang Lalu?</h1>
-        </div>
-      </div>
-      <div className="p-5 hover:scale-105 hover:bg-black/10 rounded-lg duration-500">
-        <h1 className="font-Poppins font-normal text-lg">Lorem ipsum dolor sit amet consectetur. Pulvinar viverra dictumst eleifend sed suspendisse quis. Habitasse sit ornare sed neque.</h1>
-        <div className="flex flex-row items-center text-black/60 mt-2">
-          <MdHistory className="w-5 h-5" />
-          <h1 className="font-Poppins font-light text-sm">Berapa Hari Yang Lalu?</h1>
-        </div>
-      </div>
-    </>
-  )
+    <div className="w-full space-y-2">
+      {berita.map((data, index) => {
+        return (
+          <div className="p-5 hover:bg-black/10 rounded-lg duration-500" key={index}>
+            <h1 className="font-Poppins font-normal text-lg">
+              {data.judul_berita}
+            </h1>
+            <div className="flex flex-row items-center text-black/60 mt-2">
+              <MdHistory className="w-5 h-5" />
+              <h1 className="font-Poppins font-light text-sm">
+                {data.createdAt}
+              </h1>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
