90202bd16a610a86c15e650cb1e9555dc4b8d25b
Rizki Saepul Aziz <rzkysa0@gmail.com>
2023-02-14 11:55:39 +0700

    add Lora font

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,10 @@ module.exports = {
         'BebasNeue': ['Bebas Neue', 'sans-serif'],
         'DMSans': ['DM Sans', 'sans-serif'],
         'Lato': ['Lato', 'sans-serif'],
+        'Lora': ['Lora', 'serif'],
+      },
+      colors: {
+        'primary': '#16A75C'
       }
     },
   },
